0cae9ff5c827002987db27ba485207724ea9f52e
Saiful Islam Rasel <raselcse97@gmail.com>
2019-10-05 15:46:47 +0600

    Massage print program in GO

---
+++ printMassage.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hi, I am Saiful Islam Rasel and on process learing GO\n")
+	fmt.Println("Hi, I am Saiful Islam Rasel and on process learing GO")	
+}
